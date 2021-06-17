7afa8d270da56f7c7d1b95188ee87690c8c1bc1d
Brooks Patton <brooks@brookzerker.com>
2021-06-16 21:03:15 -0600

    got it working with bbvue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,25 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <Title class="title" sub>Vue Todo</Title>
   </div>
 </template>
 
+<script>
+import {Title} from "bbvue";
+
+export default {
+  components: {
+    Title,
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+body {
+  background-color: black;
 }
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
+.title {
+  color: white;
 }
 </style>
